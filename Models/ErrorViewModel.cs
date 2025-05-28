1a0b04a58ab1d0f984fc4111ab0c5778dc6614a0
Oleksandr <nonamesan9i@gmail.com>
2025-05-28 13:35:26 +0300

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROKSRent.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
